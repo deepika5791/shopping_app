e1c0fd639aca9ba63fb50dcbec05b2740077b781
deepika5791 <deepikatripathi0810@gmail.com>
2025-11-01 19:11:28 +0530

    add to cart code changes

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -12,13 +12,42 @@ const cartProducts = async (req, res) => {
   }
 };
 
+// const addToCart = async (req, res) => {
+//   try {
+//     const { productId, quantity } = req.body;
+//     let cart = await Cart.findOne({ userId: req.user.id });
+
+//     if (!cart) {
+//       cart = new Cart({ userId: req.user.id, products: [] });
+//     }
+
+//     const existingItem = cart.products.find(
+//       (item) => item.productId.toString() === productId
+//     );
+
+//     if (existingItem) {
+//       existingItem.quantity += quantity;
+//     } else {
+//       cart.products.push({ productId, quantity });
+//     }
+
+//     await cart.save();
+//     res.json({ message: "Product added to cart", cart });
+//   } catch (error) {
+//     res.status(500).json({ message: "Server error", error: error.message });
+//   }
+// };
+
 const addToCart = async (req, res) => {
   try {
     const { productId, quantity } = req.body;
-    let cart = await Cart.findOne({ userId: req.user.id });
+
+    const userId = "guest-user";
+
+    let cart = await Cart.findOne({ userId });
 
     if (!cart) {
-      cart = new Cart({ userId: req.user.id, products: [] });
+      cart = new Cart({ userId, products: [] });
     }
 
     const existingItem = cart.products.find(
@@ -66,4 +95,3 @@ const clearCart = async (req, res) => {
 };
 
 module.exports = { cartProducts, addToCart, removeFromCart, clearCart };
-
