4a0185589e40b71c3f8570e8d32f52959393fdc9
deepika5791 <deepikatripathi0810@gmail.com>
2025-11-04 11:44:19 +0530

    env file mongoDB name changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Mongoose connected to:", mongoose.connection.name);
   } catch (error) {
     console.log(error.message);
