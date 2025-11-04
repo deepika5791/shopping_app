0306017ce0687b915fed0a8c71a42f69888b409d
deepika5791 <deepikatripathi0810@gmail.com>
2025-11-04 13:14:40 +0530

    signUp logic changes

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -5,13 +5,15 @@ const User = require("../models/User");
 const signUp = async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    if (!username || !email || password) {
-      return res.status(400).json({ error: "user not found" });
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: "All fields are required" });
     }
-    const existingUser = await User.findOne({ email: req.body.email });
+
+    const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ error: "User already exists" });
     }
+
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
@@ -20,39 +22,40 @@ const signUp = async (req, res) => {
 
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    console.error(err);
+    console.error(error);
     res.status(500).json({ error: "Server error" });
   }
 };
 
-const login = (async = async (req, res) => {
+const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(400).json({ error: "name and password required" });
+      return res.status(400).json({ error: "Email and password required" });
     }
+
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ error: "Invalid credentials" });
 
     const isMatching = await bcrypt.compare(password, user.password);
     if (!isMatching)
       return res.status(400).json({ error: "Invalid credentials" });
+
     const token = jwt.sign(
-      {
-        userId: user.id,
-        name: user.username,
-      },
+      { userId: user._id, username: user.username },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
-    req.json({
-      message: "Login Successful",
+
+    res.json({
+      message: "Login successful",
       token,
       user: { id: user._id, name: user.username, email: user.email },
     });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ error: "Server error" });
+    res.status(500).json({ error: "Server error" });
   }
-});
+};
+
 module.exports = { signUp, login };
