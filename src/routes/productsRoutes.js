7bae758552ec33cbf3c3f96836da79e44aa57e1b
deepika5791 <deepikatripathi0810@gmail.com>
2025-11-04 21:40:51 +0530

    added auth at  productRoute

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -7,13 +7,14 @@ const {
   ProductdetailEdit,
 } = require("../controller/ProductController");
 
+const auth = require("../middleware/authMIddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 const router = express.Router();
 
 router.get("/", getProducts);
 router.get("/:id", product);
-router.post("/", adminMiddleware, newProducts);
-router.delete("/:id", adminMiddleware, deleteProduct);
-router.patch("/:id", adminMiddleware, ProductdetailEdit);
+router.post("/", auth,adminMiddleware, newProducts);
+router.delete("/:id", auth, adminMiddleware, deleteProduct);
+router.patch("/:id", auth,adminMiddleware, ProductdetailEdit);
 
 module.exports = router;
