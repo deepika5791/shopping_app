51e368e2579f93e5eb931ea2627f6d0eda7f6b22
deepika5791 <deepikatripathi0810@gmail.com>
2025-11-08 18:51:44 +0530

    added get request for server status

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,6 +3,10 @@ const { signUp, login } = require("../controller/authController");
 
 const router = express.Router();
 
+router.get("/uptime", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 router.post("/signup", signUp);
 router.post("/login", login);
 
